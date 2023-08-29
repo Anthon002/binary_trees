0ba036475fd2f5640a261a92e01958a2df8a9c5f
danieljonesobot <daniel.jonesobot@gmail.com>
2023-08-29 16:21:13 +0100

    task 4

--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
 
 
 /**
@@ -29,4 +32,5 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
 #endif
