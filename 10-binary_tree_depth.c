f884ce56042c684a095b71655a3e60bd4f820867
Anthon002 <chineduanulugwo@gmail.com>
2023-08-31 06:38:05 -0700

    task 17

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,11 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - Measures the depth of a node in a binary tree.
- *
- * @tree: A pointer to the node to measure the depth.
- *
- * Return: 0 if tree is NULL.
+ * binary_tree_depth -to measure the depth of a node in binary tree.
+ * @tree: pointer to node for measuring depth.
+ * Return: 0 
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,22 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - Counts the leaves in a binary tree.
- *
- * @tree: A pointer to the root node of the tree to count the number of leaves.
- *
- * Return: 0 if tree is NULL.
+ * binary_tree_leaves -function to count the leaves in a binary tree.
+ * @tree: pointer to root node
+ * Return: 0 or leaves
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
+	size_t lvs = 0;
 
 	if (tree)
 	{
-		leaves += (!tree->left && !tree->right) ? 1 : 0;
-		leaves += binary_tree_leaves(tree->left);
-		leaves += binary_tree_leaves(tree->right);
+		lvs += (!tree->left && !tree->right) ? 1 : 0;
+		lvs += binary_tree_leaves(tree->left);
+		lvs += binary_tree_leaves(tree->right);
 	}
 
-	return (leaves);
+	return (lvs);
 }
